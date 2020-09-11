6510fcb07e47d2a145d57a854c25a825cc73caa6
mauriciomoura8 <mauricio_rockr@hotmail.com>
2020-09-11 01:13:39 -0300

    september11.1

--- Atividades/exercicios/exercicio-04/004-js/calculo.js
+++ Atividades/exercicios/exercicio-04/004-js/calculo.js
@@ -2,15 +2,32 @@ function validar(campo){
     
     let n = campo.value;
 
-    if(n.length == 0|| parseInt(n)){
-        window.alert("Valor incorreto!");
-        campo.value = "";
+    if(n.length == 0|| isNaN (parseInt(n)) || n < 18){
+        window.alert("Idade fora do permitido!");
+        campo.value = 0;
         campo.focus();
-        
+        return false
 
     }
 
+    return true;
 
+}
+
+        
+function validar2(campo2){
+    
+    let n = campo2;
+
+    if(n.length <= 3){
+        window.alert("Nome incorreto!");
+        campo.value = "0";
+        campo.focus();
+        return false
+
+    }
+
+    return true;
 
 }
 
@@ -20,9 +37,16 @@ function validar(campo){
 function calcular(){
 
         let n1 = document.frmDados.age;
-        let n2 = document.frmDados.age;
+        let n2 = document.frmDados.name;
 
-        let resultado = parseInt(n1) + parseInt(n2);
+        if(validar(n1) && validar2(n2)){
+
+        let resultado = parseInt(n1.value);
+        let resultado2 = toString(n2)
 
         document.frmDados.Resultado.value = resultado;
+        document.frmDados.Resultado2.toString = resultado2;
+        }
+
+
 }
