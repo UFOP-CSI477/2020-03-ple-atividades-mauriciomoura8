97df5c16ef11c4dcbd7266fa2fce75e9fc255399
mauriciomoura8 <mauricio_rockr@hotmail.com>
2020-09-26 17:49:25 -0300

    september26

--- Atividades/exercicios/Laravel/sistema/routes/web.php
+++ Atividades/exercicios/Laravel/sistema/routes/web.php
@@ -12,7 +12,45 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Models\Estado;
+use App\Models\Produto;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/estados/todos', function(){
+
+    $estados = Estado::all();
+    return view('lista', ['dados' => $estados]);
+});
+
+Route::get('/estados/{id}', function($id){
+
+    $estado = Estado::findOrFail($id);
+
+    if($estado == null){
+        return 'ID INVALIDO';
+    }
+    return view('lista', ['dados' => $estado]);
+
+});
+
+Route::get('/produtos/todos', function(){
+
+    $produtos = Produto::all();
+    return view('lista', ['dados' => $produtos]);
+});
+
+Route::get('/produtos/{id}', function($id){
+
+    $produto = Produto::findOrFail($id);
+
+    if($produto == null){
+        return 'ID DE PRODUTO INVALIDO';
+    }
+    return view('listaProdutos', ['dados' => $produto]);
+
+});
+
